0553027644b3a288483902d32d88f80ce24b55b6
OszkarSemerath <semerath@mit.bme.hu>
2022-07-04 19:32:17 +0200

    StatisticsUtil

---
+++ store/src/main/java/tools/refinery/store/util/StatisticsUtil.java
@@ -0,0 +1,25 @@
+package tools.refinery.store.util;
+
+import java.util.List;
+
+public class StatisticsUtil {
+	private static String delim = "\t";
+	
+	public static int sum(List<Integer> numbers) {
+		int result = 0;
+		for (int integer : numbers) {
+			result+=integer;
+		}
+		return result;
+	}
+	
+	public static void addLine(StringBuilder builder, Object... objects) {
+		for(int i=0; i<objects.length; i++) {
+			if(i != 0) {
+				builder.append(delim);
+			}
+			builder.append(objects[i]);
+		}
+		builder.append("\n");
+	}
+}
