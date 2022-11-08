2639701ca763bce51fb01ab7a4e8c3dc563a0b3b
radnotizsuzsanna <radnotizsuzsanna42@gmail.com>
2022-11-08 16:00:07 +0100

    Exception ha sérült a MapStore vagy Relation a fájlban

--- store/src/main/java/tools/refinery/store/model/ModelSerializer.java
+++ store/src/main/java/tools/refinery/store/model/ModelSerializer.java
@@ -68,39 +68,46 @@ public class ModelSerializer {
 		Map<DataRepresentation<?, ?>, VersionedMapStore<?, ?>> stores = new HashMap<>();
 
 		int i = 0;
-		while(relations.available()!=0){
-			//Relation name bolvasása
-			int length = relations.readInt();
-			byte[] nameByte = new byte[length];
-			relations.readFully(nameByte);
-			String name = new String(nameByte,"UTF-8");
-			System.out.println("\nReading Relation name: " + name);
+		try{
+			while(relations.available()!=0){
+				//Relation name bolvasása
+				int length = relations.readInt();
+				byte[] nameByte = new byte[length];
+				relations.readFully(nameByte);
+				String name = new String(nameByte,"UTF-8");
+				System.out.println("\nReading Relation name: " + name);
 
-			//Relation aritás beolvasása
-			int arity = relations.readInt();
-			System.out.println("Reading Relation arity: " + arity);
+				//Relation aritás beolvasása
+				int arity = relations.readInt();
+				System.out.println("Reading Relation arity: " + arity);
 
-			//Relation defaultValue beolvasása
-			boolean defaultValue = relations.readBoolean();
-			System.out.println("Reading Relation defaultValue: " + defaultValue);
+				//Relation defaultValue beolvasása
+				boolean defaultValue = relations.readBoolean();
+				System.out.println("Reading Relation defaultValue: " + defaultValue);
 
-			//Tuple length beolvasasa
-			int tupleLength = relations.readInt();
-			System.out.println("Reading tupleLength: " + tupleLength);
+				//Tuple length beolvasasa
+				int tupleLength = relations.readInt();
+				System.out.println("Reading tupleLength: " + tupleLength);
 
-			//Relation létrehozása
-			var relation = new Relation<>(name, arity, defaultValue);
-			System.out.println("Relation created: " + relation.getName());
+				//Relation létrehozása
+				var relation = new Relation<>(name, arity, defaultValue);
+				System.out.println("Relation created: " + relation.getName());
 
-			//VersionedMapStoreDeltaImpl létrehozása
-			DataInputStream data = streames.get(relation);
-			VersionedMapStoreDeltaImpl<?,?> mapStore = readDeltaStore(relation, data);
-			System.out.println("VersionedMapStoreDeltaImpl created.");
+				//VersionedMapStoreDeltaImpl létrehozása
+				DataInputStream data = streames.get(relation);
+				VersionedMapStoreDeltaImpl<?,?> mapStore = readDeltaStore(relation, data);
+				System.out.println("VersionedMapStoreDeltaImpl created.");
 
-			//ModelStore létrehozása Relationből és VersionedMapStoreDeltaImpl-ből
-			stores.put(relation, mapStore);
-			i++;
+				//ModelStore létrehozása Relationből és VersionedMapStoreDeltaImpl-ből
+				stores.put(relation, mapStore);
+				i++;
+			}
 		}
+		catch (IOException e){
+			if(e.getMessage().compareTo("Incomplete MapStore in file") == 0) throw e;
+			else throw new IOException("Incomplete Relation in file");
+		}
+
 
 		ModelStore store = new ModelStoreImpl(stores);
 
@@ -153,42 +160,47 @@ public class ModelSerializer {
 
 	protected VersionedMapStoreDeltaImpl<?,?> readDeltaStore(Relation<?> relation, DataInputStream data) throws IOException {
 		LongObjectHashMap<MapTransaction<Tuple, Boolean>> mapTransactionArray = new LongObjectHashMap<>();
+		try{
+			while(data.available()!=0){
+				long version = data.readLong();
+				System.out.println("\tReading version of transaction: " + version);
+				long parent = data.readLong();
+				System.out.println("\tReading parent of transaction: " + parent);
+				int num = data.readInt();
+				System.out.println("\tReading number of deltas: " + num);
 
-		while(data.available()!=0){
-			long version = data.readLong();
-			System.out.println("\tReading version of transaction: " + version);
-			long parent = data.readLong();
-			System.out.println("\tReading parent of transaction: " + parent);
-			int num = data.readInt();
-			System.out.println("\tReading number of deltas: " + num);
+				@SuppressWarnings({"unchecked"})
+				var deltas = (MapDelta<Tuple, Boolean>[]) new MapDelta[num];
+				for(int j = 0; j < num; j++){
+					//Reads the elements of the tuple
+					int[] tupleArray = new int[relation.getArity()];
+					for(int k = 0; k < relation.getArity(); k++){
+						tupleArray[k] = data.readInt();
+					}
+					Tuple tuple = Tuple.of(tupleArray);
+					System.out.println("\t\tReading tuple: " + tuple);
 
-			@SuppressWarnings({"unchecked"})
-			var deltas = (MapDelta<Tuple, Boolean>[]) new MapDelta[num];
-			for(int j = 0; j < num; j++){
-				//Reads the elements of the tuple
-				int[] tupleArray = new int[relation.getArity()];
-				for(int k = 0; k < relation.getArity(); k++){
-					tupleArray[k] = data.readInt();
+					//Reads the old and new value
+					boolean oldValue = data.readBoolean();
+					System.out.println("\t\tReading oldValue: " + oldValue);
+
+					boolean newValue = data.readBoolean();
+					System.out.println("\t\tReading newValue: " + newValue);
+					deltas[j] = new MapDelta<>(tuple, oldValue, newValue);
 				}
-				Tuple tuple = Tuple.of(tupleArray);
-				System.out.println("\t\tReading tuple: " + tuple);
-
-				//Reads the old and new value
-				boolean oldValue = data.readBoolean();
-				System.out.println("\t\tReading oldValue: " + oldValue);
-
-				boolean newValue = data.readBoolean();
-				System.out.println("\t\tReading newValue: " + newValue);
-				deltas[j] = new MapDelta<>(tuple, oldValue, newValue);
-			}
-			if(parent == -1){
-				mapTransactionArray.put(parent, new MapTransaction<>(deltas, version, null));
-			}
-			else{
-				MapTransaction<Tuple, Boolean> parentTransaction = mapTransactionArray.get(parent);
-				mapTransactionArray.put(parent, new MapTransaction<>(deltas, version, parentTransaction));
+				if(parent == -1){
+					mapTransactionArray.put(parent, new MapTransaction<>(deltas, version, null));
+				}
+				else{
+					MapTransaction<Tuple, Boolean> parentTransaction = mapTransactionArray.get(parent);
+					mapTransactionArray.put(parent, new MapTransaction<>(deltas, version, parentTransaction));
+				}
 			}
 		}
+		catch(IOException e){
+			throw new IOException("Incomplete MapStore in file");
+		}
+
 		boolean defaultValue = (boolean) relation.getDefaultValue();
 		return new VersionedMapStoreDeltaImpl<>(defaultValue, mapTransactionArray);
 	}
